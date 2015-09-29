4655da808a5e7dc8cce88744d2cd01853c0e6aa2
fay <faysvas@yahoo.com>
2015-09-29 12:59:07 +0300

    made separate page for statistics

---
+++ js/statistics.js
@@ -0,0 +1,23 @@
+  var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+];
+var ctx = $("#myChart").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+var myPieChart = new Chart(ctx).Pie(data);
